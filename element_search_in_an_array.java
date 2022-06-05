3d9d1ced83d31b62771e2e03b7bb2e07b5e153ac
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-05 07:57:19 +0530

    import java.util.Scanner; class abd{     public static void main (String args[])     {         int n,i,k,count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();
            int arr[]=new int[n];
            for(i=0;i<n;i++)
            {
                arr[i]=sc.nextInt();
            }
             k=sc.nextInt();
             for(i=0;i<n;i++)
             {
                 if(arr[i]==k)
                 {
                     count=count+1;
                 }
             }
             if(count!=0)
             {
                 System.out.println("True");
             }
             else
             {
                System.out.println("False"); 
             }
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class abd{
+    public static void main (String args[])
+    {
+        int n,i,k,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+         k=sc.nextInt();
+         for(i=0;i<n;i++)
+         {
+             if(arr[i]==k)
+             {
+                 count=count+1;
+             }
+         }
+         if(count!=0)
+         {
+             System.out.println("True");
+         }
+         else
+         {
+            System.out.println("False"); 
+         }
+    }
+}
