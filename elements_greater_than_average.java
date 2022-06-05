534f8b607f924323b535cd80bb780efadfeef431
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-05 12:08:36 +0530

    import java.util.Scanner; class abd{     public static void main(String args[])     {         int n,i,sum=0,count=0;         double avg;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();             sum=sum+x[i];         }         avg=sum/n;         for(i=0;i<n;i++)         {             if(x[i]>=avg)             count++;         }         System.out.println(count);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class abd{
+    public static void main(String args[])
+    {
+        int n,i,sum=0,count=0;
+        double avg;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+            sum=sum+x[i];
+        }
+        avg=sum/n;
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=avg)
+            count++;
+        }
+        System.out.println(count);
+    }
+}
