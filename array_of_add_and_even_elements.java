36e88d9521fb93144b3cca3deb7c60b5404d8815
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-05 11:54:11 +0530

    import java.util.Scanner; class abd{     public static void main(String arbs[])     {         int n,i;         Scanner sc=new Scanner (System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }
             for(i=0;i<n;i++)
             {
                 if(x[i]%2==1)
                 {
                     System.out.print(x[i]+" ");
                 }
             }
                for(i=0;i<n;i++)
             {
                 if(x[i]%2==0)
                 {
                     System.out.print(x[i]+" ");
                 }
             }
        }
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class abd{
+    public static void main(String arbs[])
+    {
+        int n,i;
+        Scanner sc=new Scanner (System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+         
+         for(i=0;i<n;i++)
+         {
+             if(x[i]%2==1)
+             {
+                 System.out.print(x[i]+" ");
+             }
+         }
+            for(i=0;i<n;i++)
+         {
+             if(x[i]%2==0)
+             {
+                 System.out.print(x[i]+" ");
+             }
+         }
+    }
+}
