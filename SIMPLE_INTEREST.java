80a5acf405e4547738b1d72ce67183e2a5abe202
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-22 22:01:42 +0530

    import java.util.Scanner; class abd{     public static void main(String[]arfs){         int p,t,r,am;
            Scanner sc=new Scanner(System.in);
            p=sc.nextInt();
            t=sc.nextInt();
            r=sc.nextInt();
            am=(p*t*r)/100;
            System.out.println(am);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class abd{
+    public static void main(String[]arfs){
+        int p,t,r,am;
+        
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        am=(p*t*r)/100;
+        System.out.println(am);
+    }
+}
