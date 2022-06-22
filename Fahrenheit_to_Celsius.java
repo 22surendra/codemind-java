819926e6d8005745e3fecd24b7573bad1f140534
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-22 21:58:15 +0530

    import java.util.Scanner; class abd{     public static void main(String[]arfs){         int f;         double c;         Scanner sc=new Scanner(System.in);         f=sc.nextInt();         c=(f-32)/1.8;         System.out.format("%.2f",c);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class abd{
+    public static void main(String[]arfs){
+        int f;
+        double c;
+        Scanner sc=new Scanner(System.in);
+        f=sc.nextInt();
+        c=(f-32)/1.8;
+        System.out.format("%.2f",c);
+    }
+}
