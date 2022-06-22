6b6a2e126feb42ffdabc21f54925d8af4bd1d677
22surendra <96292713+22surendra@users.noreply.github.com>
2022-06-22 22:07:46 +0530

    import java.util.Scanner; class abd{     public static void main(String[]arfs){         int n,d;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            d=(n*(n-3))/2;
            System.out.println(d);
        }
    }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class abd{
+    public static void main(String[]arfs){
+        int n,d;
+        
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        d=(n*(n-3))/2;
+        System.out.println(d);
+    }
+}
